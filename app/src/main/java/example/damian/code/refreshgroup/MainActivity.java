91830bf1f9a5d9f939d03b0636a405ea623e1e27
Poojan Shah <poojan@poojanshah.com>
2018-05-16 16:43:43 +0100

    starting moving to fragments

--- app/src/main/java/example/damian/code/refreshgroup/MainActivity.java
+++ app/src/main/java/example/damian/code/refreshgroup/MainActivity.java
@@ -8,22 +8,13 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    EditText editText;
-    Button searchButton;
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
 
-        }
-    };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.editText);
-        searchButton =  findViewById(R.id.button);
-        searchButton.setOnClickListener(onClickListener);
+
     }
 }
