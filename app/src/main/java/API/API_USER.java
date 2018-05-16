aea2424843796e871eb483d0d4fe43925c5b84d3
Andria Njoku <andriatae@gmail.com>
2018-05-16 16:10:55 +0100

    Added Main API Interface To Fetch User Info

---
+++ app/src/main/java/API/API_USER.java
@@ -0,0 +1,13 @@
+package com.example.andriatae.code_wars_project.API;
+
+import com.example.andriatae.code_wars_project.Objects.User_Container;
+
+/**
+ * Created by Andria TAE on 16/05/2018.
+ */
+
+public interface API_USER {
+
+    @GET("users/{userName}")
+    Observable<User_Container>getPlayer(@Path("userName")String userName);
+}
