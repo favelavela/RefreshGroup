83998f6e9bf2b8627b2a919a39902891c18ddb19
Poojan Shah <poojan@poojanshah.com>
2018-05-16 15:58:40 +0100

    init
    init

--- app/src/main/java/example/damian/code/refreshgroup/MainActivity.java
+++ app/src/main/java/example/damian/code/refreshgroup/MainActivity.java
@@ -2,12 +2,36 @@ package example.damian.code.refreshgroup;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    EditText editText;
+    Button searchButton;
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        searchButton =  findViewById(R.id.button);
+        searchButton.setOnClickListener(onClickListener);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
     }
 }
